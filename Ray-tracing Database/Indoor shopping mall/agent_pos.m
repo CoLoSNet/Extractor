ffec4794ae492f1f015901d4f300eea8b6491fee
CoLoSNet <155145488+CoLoSNet@users.noreply.github.com>
2024-01-02 12:17:56 +0800

    Add files via upload

---
+++ Ray-tracing Database/Indoor shopping mall/agent_pos.m
@@ -0,0 +1,8 @@
+function  Agent_set = agent_pos(Agent_set, initial, len, inter)
+
+inter2 = round(1/inter);
+col_1 = repmat([0:len(1)*inter2]', floor(len(2)*inter2)+1, 1)*inter + initial(1);
+col_2 = repmat([0:len(2)*inter2] , floor(len(1)*inter2)+1, 1)*inter + initial(2);
+Agent_set = [Agent_set ; [col_1 col_2(:)]];
+
+end
